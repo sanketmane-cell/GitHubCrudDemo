0fdced568e7ebd5808e5987695d25ef7d683bf1f
DELL <DELL@DESKTOP-NQUTE92>
2025-01-23 14:37:44 +0530

    m1 method add

---
+++ GitHubCrudDemoProject/src/demo/com/Test.java
@@ -0,0 +1,15 @@
+package demo.com;
+
+public class Test {
+
+	public void m1() {
+		
+		System.out.println("m1-----Test");
+	}
+	
+	public static void main(String[] args) {
+		
+		Test t=new Test();
+		t.m1();
+}
+}
