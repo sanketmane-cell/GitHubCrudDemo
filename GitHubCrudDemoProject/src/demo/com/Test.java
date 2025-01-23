70e6e5e70834e6a2fc1836f7cdc652a112755231
DELL <DELL@DESKTOP-NQUTE92>
2025-01-23 14:42:40 +0530

    method M2 Removed

--- GitHubCrudDemoProject/src/demo/com/Test.java
+++ GitHubCrudDemoProject/src/demo/com/Test.java
@@ -6,10 +6,6 @@ public class Test {
 		
 		System.out.println("m1-----Test");
 	}
-	public void m2() {
-		
-		System.out.println("m2-----Test");
-	}
 	
 	public static void main(String[] args) {
 		
