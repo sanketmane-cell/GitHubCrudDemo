d0527cae591149ddc95a68fef9c65d0ac3bdc7c7
sanketmane-cell <sanketmane0902@gmail.com>
2025-01-23 14:40:02 +0530

    Update Test.java
    M2 Method Add

--- GitHubCrudDemoProject/src/demo/com/Test.java
+++ GitHubCrudDemoProject/src/demo/com/Test.java
@@ -6,6 +6,10 @@ public class Test {
 		
 		System.out.println("m1-----Test");
 	}
+	public void m2() {
+		
+		System.out.println("m2-----Test");
+	}
 	
 	public static void main(String[] args) {
 		
