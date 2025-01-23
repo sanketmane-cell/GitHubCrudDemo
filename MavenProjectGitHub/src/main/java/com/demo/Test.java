3cb69eb8cf1c12ff17803edbe60b78230af41bf0
DELL <DELL@DESKTOP-NQUTE92>
2025-01-23 16:43:18 +0530

    done

--- MavenProjectGitHub/src/main/java/com/demo/Test.java
+++ MavenProjectGitHub/src/main/java/com/demo/Test.java
@@ -11,7 +11,7 @@ public class Test {
 	public static void main(String[] args) {
 		
 		ApplicationContext apc=new AnnotationConfigApplicationContext(AppConfig.class);
-			Student s=(Student)apc.getBean("s1");
+			Student s=(Student)apc.getBean("stu");
 			System.out.println(s.getRollno());
 			System.out.println(s.getName());
 			System.out.println(s.getUname());
